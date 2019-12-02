175a3730a605b37a5cf2d64e4982b97b1bdcf83d
Nick Burgin <nick.burgin@greensync.com.au>
2019-12-03 06:36:38 +1100

    Correct day 1 part 1 answer

--- 2019/fuelRequirements.go
+++ 2019/fuelRequirements.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
-	
+	var requiredFuelMass int64 = 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		row := scanner.Text()
+		payloadMass, err := strconv.ParseInt(row, 10, 64)
+		if err != nil {
+			continue
+		}
+		requiredFuelMass += (payloadMass/3)-2
+	}
+
+	fmt.Print(requiredFuelMass)
 }
